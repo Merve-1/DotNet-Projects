f8846b2244164934a4c22e17324dfd1822f91bc0
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2026-03-29 02:01:24 +0200

    Advanced C# Assignment #02

---
+++ OnlineStoreOrderProcessing/Product.cs
@@ -0,0 +1,13 @@
+namespace OnlineStoreOrderProcessing;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }//"Electronics", "Clothing", "Food", "Books"
+    public double Price { get; set; }
+    public int Stock { get; set; }
+    
+
+    
+}
